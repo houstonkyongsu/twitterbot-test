02b5491a61a1a7d7252d7d9805ab49d8638be603
Houston <houston.owen@gmail.com>
2020-09-12 19:53:56 +0100

    implementing dm functionality

--- src/botmain.py
+++ src/botmain.py
@@ -2,6 +2,7 @@ import tweepy
 import logging
 from login import create_api
 import time
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -12,6 +13,25 @@ def follow_followers(api):
         if not follower.following:
             logger.info(f"Following {follower.name}")
             follower.follow()
+
+def respond_dms(api):
+    logger.info("Replying to dms")
+    for dm in tweepy.Cursor(api.list_direct_messages(10,-1)):
+        if contains_word(dm, "hi"):
+            print("yooo \n")
+
+def contains_word(msg, word):
+    msg = str(msg)
+    try:
+        msg = msg.replace("\\","")
+        dict_msg = json.loads(msg)
+        # if 'message_create' in dict_msg.keys():
+        print(dict_msg)
+
+    except:
+        pass
+
+    return True
 
 def main():
     api = create_api()
